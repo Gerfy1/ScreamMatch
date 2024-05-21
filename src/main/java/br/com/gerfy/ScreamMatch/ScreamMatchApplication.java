48374d82a4e592dfb774ced1f577f3cc4a9c8fd8
Gerfy <gerfynlol@gmail.com>
2024-05-21 13:07:02 -0300

    Start

--- src/main/java/br/com/gerfy/ScreamMatch/ScreamMatchApplication.java
+++ src/main/java/br/com/gerfy/ScreamMatch/ScreamMatchApplication.java
@@ -17,7 +17,7 @@ public class ScreamMatchApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		var consumoAPI = new ConsumoAPI();
 		var json = 	consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=43454279");
-		//System.out.println(json);
+		//Sstem.out.println(json);
 		//json = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
 		System.out.println(json);
 		ConverteDados conversor = new ConverteDados();
